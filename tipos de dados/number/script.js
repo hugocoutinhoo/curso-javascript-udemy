3799e4e26b636ed26ab3ae14e98ff6a4d7263d59
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-11 16:37:04 -0300

    1 - number

---
+++ tipos de dados/number/script.js
@@ -0,0 +1,9 @@
+console.log(typeof (13));
+console.log(typeof (3 + 2));
+console.log(typeof (2 * 4 - 3));
+console.log(typeof (5 * (2 + 4)));
+console.log(typeof (5 / 3 + 1));
+console.log(typeof (23 % 2));
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
