25d5330289bea62581eb469de774d2ed45636eab
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-14 15:43:26 -0300

    string part2

---
+++ tipos de dados/string/script.js
@@ -0,0 +1,5 @@
+console.log(typeof 'Isso é uma string');
+console.log(typeof "Este texto também é uma string")
+var nome = "Hugo";
+console.log(`Meu nome é ${nome}`); 
+console.log('Essa é uma string \n de duas linhas.')
