d57f65fc38ad18285c3451921cdbcd639c7ae5dc
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-14 15:56:12 -0300

    booleans

---
+++ tipos de dados/boolean/script.js
@@ -0,0 +1,8 @@
+console.log(typeof true);
+console.log(typeof false);
+console.log(5 > 2);
+console.log(5 < 2);
+console.log(5 == 4);
+console.log('Hugo' === "Hugo");
+console.log(3 === "3");
+console.log(5 === 5);
