7f0a7a2ad820619215d5b1d6f0e457fdb93cd4d6
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-16 18:14:54 -0300

    funções

---
+++ estruturas-de-programacao/02_funcoes/script.js
@@ -0,0 +1,33 @@
+/* prompt()
+
+let idade = prompt("Qual é a sua idade?");
+console.log(idade);
+
+let nome = prompt("Qual é o seu nome?");
+console.log(`Olá ${nome}.`);*/
+
+
+
+/* alert()
+
+alert("Você está aprendendo JavaScript!")*/
+
+
+
+/* Math.x()*/
+
+let maior = Math.max(6,12,28,66,89);
+console.log(maior);
+
+let menor = Math.min(6,12,28,66,89);
+console.log(menor);
+
+let arredondar = Math.round(43.47);
+console.log(arredondar);
+
+
+/*console.log()*/
+
+let idade = 29;
+let nome = "Hugo"
+console.log(`O meu nome é ${nome} e tenho ${idade} anos.`)
