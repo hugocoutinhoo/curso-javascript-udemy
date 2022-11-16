24d2aa10c0c51a5caa0987a5e33481ac44f22bba
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-16 17:36:54 -0300

    variáveis

---
+++ estruturas-de-programacao/01_variaveis/script.js
@@ -0,0 +1,30 @@
+/* let name = "Hugo";
+
+console.log(name);
+console.log(`O meu nome é ${name}.`);
+
+let num = 10;
+console.log(num);
+
+num = 20;
+console.log(num);
+
+num = "trinta"
+console.log(num);
+
+let num1 = 1, num2 = 2, num3 = 3;
+console.log(num1 + num2 + num3); */
+
+/* var, let, const */
+
+var nome = "Hugo"
+const ip = "127.0.0.1"
+
+console.log(nome)
+console.log(ip)
+
+nome = "Marina"
+console.log(nome)
+
+ip - "128.0.1.2"
+console.log(ip);
