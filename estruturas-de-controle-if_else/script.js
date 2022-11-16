6d63f64b86c5294218814c47c0062d5ff9350e7d
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-16 18:37:00 -0300

    estruturas de repetição

---
+++ estruturas-de-controle-if_else/script.js
@@ -0,0 +1,11 @@
+let idade = 66;
+
+if(idade >= 18 && idade <= 65) {
+    console.log("Seu voto é obrigatório.")
+} else if (idade < 18 && idade >= 16) {
+    console.log("Seu voto é facultativo.")
+} else if (idade < 16) {
+    console.log("Você ainda não pode votar.")
+} else {
+    console.log("Seu voto é facultativo.")
+}
