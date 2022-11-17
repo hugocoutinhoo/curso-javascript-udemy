76bf7d31633db7f9c18d82777ce2bdb3d87a3fe2
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-17 11:40:25 -0300

    while; for

---
+++ estruturas-de-repeticao/script.js
@@ -0,0 +1,48 @@
+/* while
+let x = 1;
+
+while (x <= 10) {
+    console.log(x);
+    x = x + 1;
+}
+
+let y = 10;
+
+while (y > 0) {
+    console.log(y);
+    y = y - 1;
+} */
+
+
+
+/* do while 
+let x = 100;
+
+do {
+    console.log(x/2);
+    x = x - 5
+} while (x >= 0); */
+
+
+
+/* for 
+for(let num = 2; num < 100; num = num * 2) {
+    console.log("O número é: " + num);
+} */
+
+let x = 0;
+let y = 0;
+
+if(x == 0 && y == 0) {
+    let z = 5;
+    if (z > 2) {
+        console.log(z);
+    for (i = 0; i < 5; i = i + 1) {
+        console.log(i)
+        if(i == 2) {
+            console.log("i é = 2")
+        }
+    }
+}
+}
+
