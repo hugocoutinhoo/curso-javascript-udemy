f4ec724db84e6bcbc6cbb40cf3b787ddab5d21f8
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-17 17:02:32 -0300

    switch

---
+++ estruturas-de-controle-switch/script.js
@@ -0,0 +1,13 @@
+let nome = "Hugo";
+
+switch(nome) {
+    case "Hugo":
+        console.log("O nome é Hugo");
+        break;
+    case "Luiz":
+        console.log("O nome é Luiz");
+        break;
+    default:
+        console.log("O nome não foi encontrado.");
+        break;
+}
