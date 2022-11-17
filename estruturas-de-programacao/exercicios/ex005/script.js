50dc372d8bedbcb122d0c07d417a0553b1df5148
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-17 17:47:50 -0300

    ex005

---
+++ estruturas-de-programacao/exercicios/ex005/script.js
@@ -0,0 +1,9 @@
+let vel = 60;
+
+if (vel > 80) {
+    console.log("Você está acima da velocidade permitida.")
+} else if (vel < 40) {
+    console.log("Você está muito abaixo da velocidade permitida.")
+} else {
+    console.log("Você está dentro da velocidade permitida.")
+}
