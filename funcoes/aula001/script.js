5c00824f10e1534dff3dd468d18ced252de347ca
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-17 21:10:05 -0300

    funções02

---
+++ funcoes/aula001/script.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole() {
+    console.log("Olá, mundo!");
+}
+
+imprimirNoConsole()
+
+function imprimirUmNumero(num) {
+    console.log(`O número é: ${num}`);
+}
+
+imprimirUmNumero(6);
+imprimirUmNumero(3);
+imprimirUmNumero(5);
+imprimirUmNumero(8);
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
