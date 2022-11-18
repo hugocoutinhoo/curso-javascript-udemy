47e907e413732145dbacba80b93a810546b422b9
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-17 21:22:13 -0300

    ..

--- funcoes/aula002/script.js
+++ funcoes/aula002/script.js
@@ -5,15 +5,15 @@ function multiplicarTresNumeros(x,y,z) {
 console.log(multiplicarTresNumeros(2,3,4));
 
 const mult = multiplicarTresNumeros(5,6,7);
-console.log(`O valor da multiplicação é ${mult}`)
+console.log(`O valor da multiplicação é ${mult}`);
 
 function podeDirigir(idade, cnh) {
     if(idade >= 18 && cnh == true) {
-        console.log("Você tem 18 anos e já possui CNH")
+        console.log("Você tem 18 anos e já possui CNH");
     } else if (idade >= 18 && cnh == false) {
-        console.log("Você tem 18 anos mas não possui CNH")
+        console.log("Você tem 18 anos mas não possui CNH");
     } else if (idade < 18) {
-        console.log("Você não tem idade para possuir CNH.")
+        console.log("Você não tem idade para possuir CNH.");
     }
 }
 
