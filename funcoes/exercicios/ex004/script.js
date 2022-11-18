cb4af6681d0afadf1a596c1132a2bfbae687cdc6
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-18 16:19:42 -0300

    ex004

---
+++ funcoes/exercicios/ex004/script.js
@@ -0,0 +1,8 @@
+
+function numeroAleatorio(x) {
+    return Math.floor(Math.random() * x) + 1;
+}
+
+console.log(numeroAleatorio(14));
+console.log(numeroAleatorio(221));
+console.log(numeroAleatorio(381));
