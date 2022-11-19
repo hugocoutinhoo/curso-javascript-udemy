5a8f58ed984021da8bccbd79fd5ec18ba482e6a7
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-19 10:01:56 -0300

    ex008

---
+++ funcoes/exercicios/ex008/script.js
@@ -0,0 +1,10 @@
+function contadorDeCaracteres(texto) {
+    if (texto.length > 20) {
+        console.log("Texto muito longo")
+    } else {
+        console.log("Texto dentro do limite")
+    }
+}
+
+contadorDeCaracteres("Eu vou aprender a usar o JS.");
+contadorDeCaracteres("Eu te amo");
