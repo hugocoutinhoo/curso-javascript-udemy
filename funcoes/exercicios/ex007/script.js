232279369011d10dd55ba25473a2578edcf88e80
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-19 09:54:25 -0300

    ex007

---
+++ funcoes/exercicios/ex007/script.js
@@ -0,0 +1,8 @@
+function numeroPositivo(numNegativo) {
+    return Math.abs(numNegativo)
+}
+
+
+console.log(numeroPositivo(-87));
+console.log(numeroPositivo(-65));
+console.log(numeroPositivo(-37.6));
