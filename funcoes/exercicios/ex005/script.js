7d6581247fb4aad73c8d823b9fdb6bb3c107020e
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-18 23:39:11 -0300

    ex005

---
+++ funcoes/exercicios/ex005/script.js
@@ -0,0 +1,10 @@
+function idade(x) {
+    if(x >= 18) {
+        console.log("Você tem idade suficiente para entrar na auto escola.")
+    } else {
+        console.log("Você ainda não possui idade suficiente para entrar na auto escola.")
+    }
+}
+
+idade(18);
+idade(17);
