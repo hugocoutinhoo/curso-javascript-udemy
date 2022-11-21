b50934c679d1f1be58413afc0dda3afefb110808
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-21 18:58:13 -0300

    ex007

---
+++ arrays-objetos/exercicios/ex007/script.js
@@ -0,0 +1,8 @@
+let nomes = ["João", "Maria", "José", "Jesus", "Pedro"];
+
+nomes.forEach(nome => {
+    console.log(nome)
+})
+
+//for(let i = 0; i < nomes.length; i++)    { console.log(nomes[i]) }
+    
