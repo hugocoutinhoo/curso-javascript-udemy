781275e5c31e757442f7fbba2f78102640af0a5d
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-21 18:42:39 -0300

    ex005

---
+++ arrays-objetos/exercicios/ex006/script.js
@@ -0,0 +1,10 @@
+let nomes = ["João", "Maria", "José", "Hugo", "Jesus"];
+
+// console.log(nomes.includes("Hugo"))
+
+if(nomes.includes("Hugo") == true) {
+    console.log("O nome pesquisado consta na lista.")
+} else {
+    console.log("O nome pesquisado não consta na lista.")
+}
+
