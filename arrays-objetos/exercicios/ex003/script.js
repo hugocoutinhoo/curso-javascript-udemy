cd937f5e17dfbd5d6b602f17b17031cc7ec5bac8
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-21 18:19:52 -0300

    ex003

---
+++ arrays-objetos/exercicios/ex003/script.js
@@ -0,0 +1,9 @@
+let onibus = {
+    rodas: 8,
+    limiteDePassageiros: 40,
+    portas: 2,
+}
+
+console.log("O ônibus possui " + onibus.rodas + " rodas.");
+console.log("O ônibus comporta " + onibus.limiteDePassageiros + " passageiros.");
+console.log("O ônibus possui " + onibus.portas + " portas.");
