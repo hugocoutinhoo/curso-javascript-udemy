752dcde6ee578ad1aebbd08d99e7a3e4c941f4e8
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-06 22:37:40 -0300

    objetos_básico

--- arrays-objetos/aula004_objetos/script.js
+++ arrays-objetos/aula004_objetos/script.js
@@ -2,16 +2,27 @@ let pessoa = {
     nome: "Hugo",
     idade: 29,
     profissao: "Desenvolvedor",
-}
+
+    fala() {
+        console.log(`A minha idade atual é ${this.idade}.`)
+    },
+
+    incrementaIdade() {
+        this.idade++;
+    }
+};
 
 console.log(pessoa.nome)
 console.log(pessoa.profissao)
 console.log(pessoa.idade)
+pessoa.fala();
+pessoa.incrementaIdade();
+pessoa.fala();
 
 // [] - arrays
 // {} - objetos
 
-let cachorro = {
+/* let cachorro = {
     patas: 4,
     nome: "Dengo",
     cor: "Preto",
@@ -23,10 +34,10 @@ let cachorro = {
 console.log(cachorro.patas);
 console.log(cachorro.nome);
 console.log(cachorro.cor);
-cachorro.latir();
+cachorro.latir(); */
 
 //// Adicionando e deletando propriedades ao longo do programa:
-let pessoa2 = {
+/* let pessoa2 = {
     nome: "Luiz",
     idade: 29,
     profissao: "Estudante"
@@ -39,4 +50,17 @@ console.log(pessoa2.nome);
 //adicionando propriedade:
 pessoa2.nome = "Luiz"
 
-console.log(pessoa2.nome);
+console.log(pessoa2.nome); */
+
+//----------------------------------------------------------------------
+
+function criaPessoa (nome, sobrenome, idade) {
+    return {nome, sobrenome, idade};
+}
+const pessoa1 = criaPessoa('Hugo', 'Luiz', 29);
+const pessoa2 = criaPessoa('Maria', 'Luiza', 32);
+const pessoa3 = criaPessoa('João', 'Costa', 19);
+
+console.log(pessoa1.nome);
+console.log(pessoa2.sobrenome);
+console.log(pessoa3.idade);
