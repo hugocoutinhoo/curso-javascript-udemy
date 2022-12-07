da1c5a1a66ebcffe5dbc0621fa8bd06f175270b6
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-06 21:44:33 -0300

    array_básico

--- arrays-objetos/aula001_arrays/script.js
+++ arrays-objetos/aula001_arrays/script.js
@@ -1,9 +1,9 @@
-let arr = [1,4,7,8,9];
+/* let arr = [1,4,7,8,9];
 let nomes = ["Hugo", "Luiz", "Mirelly", "Moura"];
 let bool = [true, false, true, false, true];
-let mix = ["Hugo", 29, true, "Mirelly", 26];
+let mix = ["Hugo", 29, true, "Mirelly", 26]; */
 
-console.log(arr)
+/* console.log(arr)
 console.log(nomes)
 console.log(bool)
 console.log(mix)
@@ -12,4 +12,21 @@ console.log(arr[4])
 console.log(nomes[2])
 console.log(bool[0])
 console.log(mix[1])
-console.log(nomes[nomes.length - 1]) // último elemento do array
+console.log(nomes[nomes.length - 1]) // último elemento do array */
+
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos instanceof Array);
+
+//alunos.push('Luíza');
+//alunos.push('Eduardo');
+//console.log(alunos.slice(0,3)); //último elemento não é incluído
+
+
+//delete alunos[1];
+//console.log(alunos);
+
+//alunos.unshift('Luíza'); -> Adiciona no começo
+//alunos.push('Otávio'); -> Adiciona no fim
+//alunos.pop(); -> Retira o último array
+//alunos.shift(); -> Retira o primeiro array
