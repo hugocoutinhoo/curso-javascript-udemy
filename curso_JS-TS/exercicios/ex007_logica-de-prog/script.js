b46bd6ce5dd428808e49ba084fae0742fd9a00b2
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-13 17:10:35 -0300

    ex007

---
+++ curso_JS-TS/exercicios/ex007_logica-de-prog/script.js
@@ -0,0 +1,25 @@
+/* function ePaisagem(largura, altura) {
+    return largura > altura ? true : false;
+}
+
+console.log(ePaisagem(1920, 1080));
+console.log(ePaisagem(1080, 1920)); */
+
+
+//------------------------------------------------------------
+/* function ePaisagem(largura, altura) {
+    return largura > altura;
+}
+
+console.log(ePaisagem(1920, 1080));
+console.log(ePaisagem(1080, 1920)); */
+
+
+
+//--------------------------------------arrow function--------
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+
+console.log(ePaisagem(1920, 1080));
+console.log(ePaisagem(1080, 1920));
