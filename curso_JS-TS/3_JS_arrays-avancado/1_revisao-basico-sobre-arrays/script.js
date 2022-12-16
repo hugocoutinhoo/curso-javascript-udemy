da58f5bca192e9cdf18df59f7993aedc14ddc6dc
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-16 14:40:03 -0300

    revisão básica sobre arrays

---
+++ curso_JS-TS/3_JS_arrays-avancado/1_revisao-basico-sobre-arrays/script.js
@@ -0,0 +1,23 @@
+
+//------construtor----------------
+//string, objetos, funções, números
+//const nomes = new Array('Eduardo', 'Maria', 'Joana');
+
+
+//-----forma literal--------------
+//const nomes = ['Hugo', 'Luiz', 'Joana', 'Wallace', 'Rosana'];
+
+//const novo = nomes.slice(0, -1);
+
+//console.log(nomes, novo);
+
+//----------------------------------
+//converter strings em um array:
+const nome = 'Luiz Otávio Miranda';
+const nomes = nome.split(' ');
+console.log(nomes);
+
+//converter array em uma string:
+/* const nomes =  ['Luiz', 'Otávio', 'Miranda' ]
+const nome = nomes.join(' ');
+console.log(nome); */
