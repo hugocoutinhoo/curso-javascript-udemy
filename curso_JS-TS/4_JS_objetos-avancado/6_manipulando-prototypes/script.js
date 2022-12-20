82f7b52e5e2a6086a7cb125471782f61d0d41aea
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-20 16:36:55 -0300

    manipulando prototypes

---
+++ curso_JS-TS/4_JS_objetos-avancado/6_manipulando-prototypes/script.js
@@ -0,0 +1,60 @@
+/*
+JavaScript é baseado em protótipos para passar propriedades e métodos de um objeto para outro.
+
+Definição de protótipo:
+Protótipo é o termo usado para se referir ao que foi criado pela primeira vez, servindo de modelo ou molde para futuras produções.
+
+Todos os objetos tem um referência interna para um protótipo (__proto__) que vem da propriedade prototype da função construtora que foi usada para criá-lo. Quando tentamos acessar um membro de um objeto, primieroa o motor do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
+*/
+
+// Manipulando Prototypes:
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+// objeto literal:
+const p2 = {
+    nome: 'caneca',
+    preco: 15
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.aumento(10);
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 99
+    },
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42
+    }
+});
+p3.aumento(10);
+
+console.log(p3);
+
+
+
+
+
+
+
+
+
