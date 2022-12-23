74ce0d43c9295b2f38b794019e3893b1c41cb463
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-12-23 19:07:15 -0300

    aulas sobre Métodos úteis para Promises pt1

---
+++ curso_JS-TS/6_JS-assincrono-promises-ajax-fetch-async-await/2_metodos-uteis-promises/script.js
@@ -0,0 +1,20 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject('BAD VALUE');
+        
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+    }
+
+
+    
+
+
